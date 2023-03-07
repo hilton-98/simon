6f395f8f6d07c45bd170047ee0c2509ce8e1d17e
Kevin Hilton <hiltonk98@gmail.com>
2023-03-06 21:58:27 -0700

    Added play.js (bare bones) and code to grab name from localStorage

--- login.js
+++ login.js
@@ -1,5 +1,7 @@
 function login() {
-    const name = document.querySelector("#name");
+    const name = document.getElementById("name");
+    console.log(name);
+    console.log(name.value);
     localStorage.setItem("userName", name.value);
     window.location.href = "play.html";
 }
